be4cfb2df7b182d8f5bf250df79f68679b17e46c
O-Gu <102757670+O-Gu@users.noreply.github.com>
2022-04-21 14:59:14 -0700

    Session 1 prompt for Coding Journal 1

---
+++ NameFave.py
@@ -0,0 +1,14 @@
+#The following codes will print out my name pronouns and what my favorit movie and food is
+#the following code will tell computure to print my name & pronouns 
+
+def NamePronouns():
+	print("My name is Ofelia Gualotuna Barbalena (she/her)")
+	print("My favorite movie is Monster Inc.\nMy favorite food is Consome de Barbacoa ")
+	
+
+#defining the main function
+def main():
+	NamePronouns()
+if __name__=='__main__':
+	main()
+
